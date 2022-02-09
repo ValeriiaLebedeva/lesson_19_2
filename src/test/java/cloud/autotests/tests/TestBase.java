6f35f6a46a3407a02760491729c05c3b2760b700
Vova Oleynik <oleynik.96@gmail.com>
2022-02-09 12:25:03 +0200

    refactored tests

--- src/test/java/cloud/autotests/tests/TestBase.java
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -8,9 +8,12 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static cloud.autotests.tests.demowebshop.AddToCartUsingApiTest.webShopConfig;
 
 
 @ExtendWith({AllureJunit5.class})
@@ -19,6 +22,7 @@ public class TestBase {
     static void setUp() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         DriverSettings.configure();
+        RestAssured.baseURI = webShopConfig.apiUrl();
     }
 
     @AfterEach
